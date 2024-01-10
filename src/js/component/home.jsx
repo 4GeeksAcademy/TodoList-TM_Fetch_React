4f17259fe1069a8d4be90ddbf1642c1828c78e65
tomasmj503 <86091325+tomasmj503@users.noreply.github.com>
2024-01-10 00:21:56 +0000

    Todo list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,54 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import { useState } from "react";
+import "/workspaces/TodoList-TM/src/styles/home.css"
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+    const [input, setInput] = useState("")
+    const [tareas, setTareas] = useState([])
+ 
+
+    function manejoEnvio(e) {
+        e.preventDefault()
+        const tareasActualizadas = [input, ...tareas]
+        setTareas(tareasActualizadas)
+        setInput("")
+    }
+
+    function borrar(id) {
+        let nuevo = []
+        nuevo = tareas.filter((item, index) => {
+            if(index != id) {
+                return item
+            }
+        } )
+        setTareas(nuevo)
+    }
+
+    return (
+        <div className="tarea-contenedor">
+            <form className="tarea-formulario"
+            onSubmit={manejoEnvio}>
+                <input
+                className="tarea-input"
+                type="text" 
+                placeholder="Escribe una tarea"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                />
+            </form>
+            <ul className="list-unstyled ">
+                {tareas.map((elemento, id) => ( 
+                    <li key={id}> 
+                
+                {id} - {elemento} 
+                <button onClick={() => borrar(id)}>x</button>
+                </li>
+                ))}
+            </ul>
+
+            <p>Tareas pendientes: {tareas.length} </p>
+        </div>       
+    )
+}
 
 export default Home;
